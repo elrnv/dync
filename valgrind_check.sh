69b03124254d840d22d8f8a4efe4f10dec9ab7c3
Egor Larionov <egor.larionov@gmail.com>
2020-10-01 19:48:38 -0700

    Update valgrind check and add miri to ci

--- valgrind_check.sh
+++ valgrind_check.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-
-export bin=$(cargo test 2>&1 | grep "Running.*target/debug/deps/.*" | sed 's/.*Running //g')
-valgrind --error-exitcode=1 --leak-check=full ./$bin
+cargo test 2>&1 | grep "Running.*target/debug/deps/.*" | sed 's/.*Running //g' | xargs -I{} valgrind --error-exitcode=1 --leak-check=full ./{}
