0e132c88b1d6b17654b833eeb7b65234b3b1e7ae
Egor Larionov <egor.larionov@gmail.com>
2021-11-15 14:49:17 -0800

    Fix valgrind script
    It depended on some output structure that has changed in recent versions
    of the rust compiler.

--- valgrind_check.sh
+++ valgrind_check.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cargo test 2>&1 | grep "Running.*target/debug/deps/.*" | sed 's/.*Running //g' | xargs -I{} valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./{}
+cargo test 2>&1 | grep "Running.*target/debug/deps/.*" | sed 's/.*Running .*(\(.*\))/\1/g' | xargs -I{} valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./{}
